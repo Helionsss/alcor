abec50d7acff5c319e42a3a9c39c5b47f2551200
KitchiDioless <dustirris@gmail.com>
2023-09-26 23:46:27 +0300

    some args

--- alcor/lab_1.cpp
+++ alcor/lab_1.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <string>
 #include <fstream>
-#include <vector>
+#include <string>
 
 
 void print(std::string filename, int lines, bool tail, char delimiter) {
@@ -12,14 +11,14 @@ void print(std::string filename, int lines, bool tail, char delimiter) {
         if (lines >= 0) {
             int count = 0;
             while (std::getline(file, temp, delimiter)) {
+                if (count == lines) {
+                    break;
+                }
                 std::cout << temp;
                 if (!file.eof()) {
                     std::cout << delimiter;
                 }
                 count += 1;
-                if (count == lines) {
-                    break;
-                }
             }
         }
         else {
@@ -42,11 +41,24 @@ int main(int argc, char* argv[]) {
 
     for (int i = 1; i < argc; i++) {
         std::string arg = argv[i];
+
         if (arg == "-l") {
-            lines = stoi(std::string(argv[++i]));
+            if (stoi(arg.substr(8)) < 0) {
+                std::cout << "Error: --lines value must be only positive number";
+                exit(0);
+            }
+            else {
+                lines = stoi(std::string(argv[++i]));
+            }
         }
         else if (arg == "--lines=") {
-            lines = stoi(arg.substr(8));
+            if (stoi(arg.substr(8)) < 0) {
+                std::cout << "Error: --lines value must be only positive number";
+                exit(0);
+            }
+            else {
+                lines = stoi(arg.substr(8));
+            }
         }
         else if ((arg == "-t") or (arg == "--tail")) {
             tail = true;
@@ -54,18 +66,21 @@ int main(int argc, char* argv[]) {
         else if (arg == "-d") {
             i++;
             if (std::string(argv[i]) == "-l" or argv[i] == "-t" or argv[i] == "--tail" or argv[i] == "--lines=") {
-                std::cout << "Error";
+                std::cout << "Error: set the values of all arguments";
                 exit(0);
             }
             else {
                 if (argv[i][0] == '\'' or argv[i][0] == '"') {
                     if (argv[i][1] == '\'' or argv[i][1] == '"') {
-                        std::cout << "Error";
+                        std::cout << "Error: wrong input format";
                         exit(0);
                     }
                     else {
                         delimiter = argv[i][1];
                     }
+                }
+                else if (argv[i][0] == '\\') {
+                    delimiter = argv[i][0] + argv[i][1];
                 }
                 else {
                     delimiter = argv[i][0];
@@ -75,7 +90,7 @@ int main(int argc, char* argv[]) {
         else if (arg == "--delimiter="){
             std::cout << 1;
             if (std::string(argv[i]) == "-l" or argv[i] == "-t" or argv[i] == "--tail" or argv[i] == "--lines=") {
-                std::cout << "Error";
+                std::cout << "Error: set the values of all arguments";
                 exit(0);
             }
             else {
@@ -84,12 +99,14 @@ int main(int argc, char* argv[]) {
             }
         }
         else {
-            std::cout << arg;
+            std::cout << argv[i][0];
             filename = argv[i];
         }
     }
 
     std::cout << "/ " << delimiter << " " << ((tail == 1) ? ("true ") : ("false ")) << lines << " /" << "\n";
+
     print(filename, lines, tail, delimiter);
+
     return 0;
 }
